8fe3bc4bb2412a7385be42e2a21fcf328640fd50
IDcodea <arismunandar1194@gmail.com>
2022-10-14 17:25:11 -0600

    add children

---
+++ src/components/testing.jsx
@@ -0,0 +1,12 @@
+//children component
+import React from "react"
+
+const Testing = () => {
+  return (
+    <React.Fragment>
+      <h1>Testing</h1>
+      <p>Hello Testing</p>
+    </React.Fragment>
+  )
+}
+export default Testing
