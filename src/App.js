12efd17ccf1f6b6579d2d6e513ae12a1cfeacb33
IDcodea <arismunandar1194@gmail.com>
2022-10-14 19:26:07 -0600

    set useState

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 // import logo from './logo.svg';
 import './App.css';
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import Testing from './components/testing';
 
 
 const App = () => {
+  const [limit, setLimit] = useState(1)
+  const [name, setName] = useState('Aris M')
+  const inputNameRef=useRef()
+
+  useEffect(() => {
+    setName(inputNameRef.current.value);
+  }, [limit])
+
   return (
     <>
-      <Testing/>
+      <center>
+        {name}{limit}
+        <br />
+        <input placeholder='input disini' ref={inputNameRef} />
+        <button onClick={() => setLimit((prev) => prev + 1)}>naikkan limit</button>
+        <button onClick={() => setLimit((prev) => prev - 1)}>turunkan limit</button>
+      </center>
+      <Testing />
     </>
   )
 }
